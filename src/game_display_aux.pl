d2c057b4d93eb151a752d73663e7ac0a748bcdc5
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2024-12-26 00:13:42 +0000

    tentar ver melhor como fazer e como organizar as cenas

---
+++ src/game_display_aux.pl
@@ -0,0 +1,2 @@
+% everything that has to do with displaying the game (works a bit like a view in MVC (Model View Controller) design pattern)
+
---
+++ src/game_state_aux.pl
@@ -0,0 +1,2 @@
+% everything that has to do with accessing and changing the game_state (works a bit like a model in MVC (Model View Controller) design pattern)
+
